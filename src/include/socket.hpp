973189398f266782fd083de0978f73058ac2ae72
Danilo Luvizotto <daniloluvizotto.l@thoughtworks.com>
2019-04-30 14:24:59 -0300

    CMake: tests support included

--- src/include/socket.hpp
+++ src/include/socket.hpp
@@ -1,10 +1,19 @@
 #pragma once
 
-#include <sys/socket.h>
+#include <netinet/in.h>
 
 class Socket
 {
 public:
     Socket();
     virtual ~Socket();
+
+    bool Ready() { return _ready; }
+
+private:
+    struct sockaddr_in _addr;
+	char _send_buff[1025];
+    int _listen_fd;
+    int _conn_fd;
+    bool _ready = false;
 };
--- src/socket.cpp
+++ src/socket.cpp
@@ -1,8 +1,46 @@
 #include "socket.hpp"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
 
-Socket::Socket()
+Socket::Socket() : _listen_fd(0), _conn_fd(0)
 {
+	memset(&_addr, 0, sizeof(_addr));
+	memset(_send_buff, 0, sizeof(_send_buff));
+    (void)_listen_fd; // TODO remove this
+    (void)_conn_fd;
 
+    /*
+
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(5000);
+
+	bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+
+	listen(listenfd, 10);
+
+	while(1)
+	{
+		connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+
+		ticks = time(NULL);
+		snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
+		write(connfd, sendBuff, strlen(sendBuff));
+
+		close(connfd);
+		sleep(1);
+    }
+    */
 }
 
 Socket::~Socket()
